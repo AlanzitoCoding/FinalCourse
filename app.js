1fe0df4a900b410b5e66ad85b3a2b5241ed45073
GabrielFraga <87135402+Gab1203@users.noreply.github.com>
2024-09-19 16:59:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,84 @@
 const express = require('express');
-const app = express();
-const conn = require('./db');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const path = require('path');
 
+const app = express();
 const port = 8081;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/HTMLs/home.html');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'cimatec',
+    database: 'curso_online'
 });
 
-app.get('/loginScreen', function(req, res){
-    res.sendFile(__dirname + '/HTMLs/login.html');
-});
-
-app.get('/registerScreen', function(req, res){
-    res.sendFile(__dirname + '/HTMLs/register.html');
-});
-
-app.post('/registerUser', (req, res) => {
-    const { email, password } = req.body || {};
-
-    if (!email || !password) {
-        return res.status(400).send('Email and password are required.');
+db.connect((err) => {
+    if (err) {
+        throw err;
     }
+    console.log('Conectado ao banco de dados!');
+});
 
-    const query = 'INSERT INTO users (userEmail, userPassword) VALUES (?, ?)';
-    
-    conn.query(query, [email, password], (err, result) => {
+app.use(express.static(path.join(__dirname, 'HTMLs')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTMLs', 'register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTMLs', 'login.html'));
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTMLs', 'home.html'));
+});
+
+app.post('/submit', (req, res) => {
+    const { gmail, senha } = req.body;
+    const query = 'INSERT INTO usuario (gmail, senha) VALUES (?, ?)';
+    db.query(query, [gmail, senha], (err, result) => {
         if (err) {
-            console.error('Insertion Error: ' + err.stack);
-            return res.status(500).send('Registration Error.');
+            return res.status(500).json({ message: 'Erro ao realizar cadastro!' });
         }
-
-        res.send('User has been successfully registered.');
+        res.json({ message: 'Cadastro realizado!' });
     });
 });
 
-app.post('/email', (req, res) => {
-    const {email, password} = req.body;
-    const query = 'select * from users where userEmail = ? and userPassword = ?';
-
-    conn.query(query, [email, password], (err, result) => {
-        if(err){
-            console.error('User not found: ' + err.stack);
-            return res.status(500).send('User not found.');
+app.get('/getData', (req, res) => {
+    const query = 'SELECT * FROM usuario';
+    db.query(query, (err, results) => {
+        if (err) {
+            return res.status(500).json({ message: 'Erro ao recuperar dados!' });
         }
-        else{
-            res.json({message: "Logged in!"});
-        }
+        res.json(results);
     });
 });
 
-app.listen(port, function(){
-    console.log('Server is working properly!');
+app.post('/auth', (req, res) => {
+    const { gmail, senha } = req.body;
+
+    if (gmail && senha) {
+        db.query('SELECT * FROM usuario WHERE gmail = ? AND senha = ?', [gmail, senha], (err, results) => {
+            if (err) {
+                return res.status(500).json({ message: 'Erro na autenticação!' });
+            }
+
+            if (results.length > 0) {
+                
+                res.redirect('/home');
+            } else {
+                res.status(401).send('Gmail ou senha incorretos!');
+            }
+        });
+    } else {
+        res.status(400).send('Digite um gmail e senha!');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
