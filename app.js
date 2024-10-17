766c6a477eb0884a4ba498458407a983e8297a18
GabrielFraga <gabrielbff2006@gmail.com>
2024-10-16 21:43:20 -0300

    firebase Login

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
 const path = require('path');
 const firebase = require('firebase/app')
 const auth = require('firebase/auth')
@@ -16,30 +15,13 @@ const firebaseConfig = {
 
 }
 
-
-const app = express();
-const port = 8081;
-
 firebase.initializeApp(firebaseConfig)
 
 
+const app = express();
+const port = 8081;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '2006Pa#*#',
-    database: 'xNodeDB'
-});
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Conectado ao banco de dados!');
-});
-
 app.use(express.static(path.join(__dirname, 'HTMLs')));
 
 app.get('/', (req, res) => {
@@ -66,16 +48,7 @@ app.get('/CursoGithub', function(req, res){
     res.sendFile(__dirname + '/HTMLs/CursoGithub.html');
 });
 
-/*app.post('/submit', (req, res) => {
-    const { gmail, senha } = req.body;
-    const query = 'INSERT INTO usuario (gmail, senha) VALUES (?, ?)';
-    db.query(query, [gmail, senha], (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: 'Erro ao realizar cadastro!' });
-        }
-        res.redirect('/loginScreen');
-    });
-});*/
+
 
 app.post('/submit', (req, res) => {
     const { gmail, senha } = req.body;
@@ -92,35 +65,25 @@ res.send('Conta cadastrada com sucesso!')
        
 });
 
-app.get('/getData', (req, res) => {
-    const query = 'SELECT * FROM usuario';
-    db.query(query, (err, results) => {
-        if (err) {
-            return res.status(500).json({ message: 'Erro ao recuperar dados!' });
-        }
-        res.json(results);
-    });
-});
+
+
+
 
 app.post('/auth', (req, res) => {
     const { gmail, senha } = req.body;
 
-    if (gmail && senha) {
-        db.query('SELECT * FROM usuario WHERE gmail = ? AND senha = ?', [gmail, senha], (err, results) => {
-            if (err) {
-                return res.status(500).json({ message: 'Erro na autenticação!' });
-            }
+    
+       
+        auth.signInWithEmailAndPassword(auth.getAuth(), gmail, senha)
+        .then((userCredential) => {
 
-            if (results.length > 0) {
-                
-                res.redirect('/Cursos');
-            } else {
-                res.status(401).send('Gmail ou senha incorretos!');
-            }
-        });
-    } else {
-        res.status(400).send('Digite um gmail e senha!');
-    }
+            res.redirect('/Cursos');
+
+        })
+        .catch((error) => {
+            res.status(500).json({ message: 'Erro na autenticação!' });
+        })       
+    
 });
 
 app.listen(port, () => {
